e2505ebd22c7860e0e7204dca92f1ea9b8547147
James Astell <james.a.astell@gmail.com>
2023-03-23 19:23:13 +0000

    Create list of all bookings for one email (not displaying)
    - Retrieve all guest bookings and compare to submitted email.
    
    - For each email that matches gather ALL objects.
    
    - At end of loop add to list of objects to be displayed by HTML.
    
    - Doesn't work and can't get multiple th:each to work.

--- src/main/java/com/sg/propertyWebsite/controllers/GuestController.java
+++ src/main/java/com/sg/propertyWebsite/controllers/GuestController.java
@@ -5,6 +5,7 @@ import com.sg.propertyWebsite.daos.BookingDao;
 import com.sg.propertyWebsite.daos.BookingDaoDB;
 import com.sg.propertyWebsite.daos.GuestDaoDB;
 import com.sg.propertyWebsite.daos.PropertyDaoDB;
+import com.sg.propertyWebsite.entities.Amenity;
 import com.sg.propertyWebsite.entities.Booking;
 import com.sg.propertyWebsite.entities.Guest;
 import com.sg.propertyWebsite.entities.Property;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class GuestController {
@@ -31,28 +34,47 @@ public class GuestController {
     BookingService bookingService;
 
     @GetMapping("customerBookings")
-    public String displayBooking(Model model, String email, @ModelAttribute("b") Booking b) {
-        Guest g = guestDao.getGuestByEmail(email);  // THIS WON't WORK IF MORE THAN 1 EMAIL IN DB
-        int guestID = g.getGuestID();
+    public String displayBooking(Model model, String email, @ModelAttribute("b") Booking booking) {
+        List<Guest> allGuestBookings = new ArrayList<>();
+        List<Property> allPropertyBookings = new ArrayList<>();
+        List<Amenity> allAmenityBookings = new ArrayList<>();
+        List<Booking> allBookingBookings = new ArrayList<>();
 
-        b = bookingDao.getBookingByGuestId(guestID);
-        int propertyID = b.getPropertiesID();
+        List<Guest> allGuests = guestDao.getAllGuests();  // Get all guests from DB
 
+        // Loop through to see where email == email
+        for (Guest i : allGuests) {
+            if (Objects.equals(i.getEmail(), email)) {
+                // if email entered matches email on guest account retrieve full guest
+                Guest guest = guestDao.getGuestByID(i.getGuestID());
 
-        Property p = propertyDao.getPropertyByID(propertyID);
-        List<String> a = propertyDao.getAmmentiesByID(p.getAmmenitiesID());
+                booking = bookingDao.getBookingByGuestId(i.getGuestID());
 
-        b.setGuestID(guestID);
-        b.setPropertiesID(propertyID);
-        b = bookingService.calculateTotalCost(b);
-        model.addAttribute("g", g);
-        model.addAttribute("p", p);
-        model.addAttribute("a", a);
-        model.addAttribute("b",b);
+                int propertyID = booking.getPropertiesID();
+                Property property = propertyDao.getPropertyByID(propertyID);
 
+                List<String> ammenties = propertyDao.getAmmentiesByID(property.getAmmenitiesID());
+
+                booking.setGuestID(i.getGuestID());
+                booking.setPropertiesID(propertyID);
+                booking = bookingService.calculateTotalCost(booking);
+
+                allGuestBookings.add(guest);
+                allPropertyBookings.add(property);
+                //allAmenityBookings.add(ammenties);
+                allBookingBookings.add(booking);
+            }
+        }
+
+        // Need to return List of each element
+        model.addAttribute("g", allGuestBookings);
+        model.addAttribute("p", allPropertyBookings);
+        model.addAttribute("a", allAmenityBookings);  // Currently not working as ammenties is a list not a object but I will make another function later
+        model.addAttribute("b", allBookingBookings);
 
         return "customerBookings";
     }
+
 
 }
 
